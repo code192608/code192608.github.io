c3aa3a36c16ba1ca7a211c48f0f1fa40a0346d5e
code192608 <1814891804@qq.com>
2020-05-01 20:50:18 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,20 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+git branch -D master
+
+git checkout -b master
+
+rm -rf node_modules
+
 yarn install
 # 生成静态文件
 yarn build
-# 进入生成的文件夹
-cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f origin master
