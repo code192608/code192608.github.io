79b3cf0e7c357f229d1f4f9ac7bcccbfc66bcbdb
code192608 <1814891804@qq.com>
2020-05-02 09:25:22 +0800

    添加百度统计,修复阅读量展示,修改导航关于为留言板

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,27 +1,43 @@
 module.exports = {
   locales: {
-    '/': {
-      lang: 'zh-CN'
-    }
+    "/": {
+      lang: "zh-CN",
+    },
   },
   title: "XShare",
-  description: '分享，让世界更美好...',
-  dest: 'public',
+  description: "分享，让世界更美好...",
+  dest: "public",
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
+    [
+      ("script",
+      {},
+      `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?121691850397bf2a9201e94d724e417c";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+        `),
+    ]
   ],
-  theme: 'reco',
+  theme: "reco",
   themeConfig: {
     nav: [
-      { text: '首页', link: '/', icon: 'reco-home' },
-      { text: '时间线', link: '/timeline/', icon: 'reco-date' },
-      { text: '关于', 
-        icon: 'reco-message',
-        link: '/about/'
-      }
+      { text: "首页", link: "/", icon: "reco-home" },
+      { text: "时间线", link: "/timeline/", icon: "reco-date" },
+      { text: "留言板", icon: "reco-message", link: "/leavecomment/" },
     ],
-    type: 'blog',
+    type: "blog",
     // 博客设置
     blogConfig: {
       category: {},
@@ -35,29 +51,29 @@ module.exports = {
       //   link: ''
       // }
     ],
-    logo: '/logo.png',
+    logo: "/logo.png",
     // 搜索设置
     search: true,
     searchMaxSuggestions: 10,
     // 自动形成侧边导航
-    sidebar: 'auto',
+    sidebar: "auto",
     // 最后更新时间
-    lastUpdated: 'Last Updated',
+    lastUpdated: "Last Updated",
     // 作者
-    author: 'XShare',
+    author: "XShare",
     // 作者头像
-    authorAvatar: '/avatar.png',
+    authorAvatar: "/avatar.png",
     // 项目开始时间
-    startYear: '2020',
+    startYear: "2020",
     /**
      * valine 设置 (if you need valine comment )
      */
     valineConfig: {
-      appId: 'toL0XHRivHc7w0XIJmwapfSN-gzGzoHsz',
-      appKey: 'q1i08aGF997WVj8pjXUcUzik', // your appKey
-    }
+      appId: "toL0XHRivHc7w0XIJmwapfSN-gzGzoHsz",
+      appKey: "q1i08aGF997WVj8pjXUcUzik", // your appKey
+    },
   },
   markdown: {
-    lineNumbers: true
-  }
-}  
+    lineNumbers: true,
+  },
+};
